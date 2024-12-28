4d08064de06ed4dce297780be6f35dc380f034ad
Dan Foreman-Mackey <foreman.mackey@gmail.com>
2024-12-28 19:45:49 +0000

    stuff

--- recipe/build_finufft.sh
+++ recipe/build_finufft.sh
@@ -2,32 +2,11 @@
 
 set -euxo pipefail
 
-if [[ $ARCH == "arm64" ]]; then
-
-    if [[ $PKG_NAME == "finufft" ]]; then
-
-        SKBUILD_CMAKE_DEFINE=FINUFFT_ARCH_FLAGS="-mcpu=apple-m3" FINUFFT_USE_OPENMP=ON \
-            "${PYTHON}" -m pip install --no-deps --no-build-isolation -vv ./python/finufft
-
-    elif [[ $PKG_NAME == "cufinufft" ]]; then
-
-        SKBUILD_CMAKE_DEFINE=FINUFFT_ARCH_FLAGS="-mcpu=apple-m3" FINUFFT_USE_OPENMP=ON \
-            "${PYTHON}" -m pip install --no-deps --no-build-isolation -vv ./python/cufinufft
-
-    fi
-
+if [[ "${target_platform}" == "osx-arm64" ]]; then
+  export ARCH_FLAGS="-mcpu=apple-m3"
 else
-
-    if [[ $PKG_NAME == "finufft" ]]; then
-
-        SKBUILD_CMAKE_DEFINE=FINUFFT_ARCH_FLAGS="-m${ARCH}" FINUFFT_USE_OPENMP=ON \
-            "${PYTHON}" -m pip install --no-deps --no-build-isolation -vv ./python/finufft
-
-    elif [[ $PKG_NAME == "cufinufft" ]]; then
-
-        SKBUILD_CMAKE_DEFINE=FINUFFT_ARCH_FLAGS="-m${ARCH}" FINUFFT_USE_OPENMP=ON \
-            "${PYTHON}" -m pip install --no-deps --no-build-isolation -vv ./python/cufinufft
-
-    fi
-
+  export ARCH_FLAGS="-m${ARCH}"
 fi
+
+SKBUILD_CMAKE_DEFINE="FINUFFT_ARCH_FLAGS=${ARCH_FLAGS} FINUFFT_USE_OPENMP=ON" \
+  "${PYTHON}" -m pip install --no-deps --no-build-isolation -vv "./python/${PKG_NAME}"
