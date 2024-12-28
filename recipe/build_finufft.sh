64bf253930b221086d3d94ebda43ed9442ed629b
Dan Foreman-Mackey <dfm@dfm.io>
2024-12-28 20:31:35 +0000

    Update to 2.3.1 (#6)
    * updated v2.3.1
    
    * MNT: Re-rendered with conda-build 24.9.0, conda-smithy 3.44.3, and conda-forge-pinning 2024.11.12.14.24.54
    
    * No patches
    
    * hackin
    
    * add make
    
    * make
    
    * static
    
    * arch
    
    * script
    
    * arch
    
    * openmp
    
    * win
    
    * arm
    
    * stuff
    
    * env var
    
    * suffix
    
    * MNT: Re-rendered with conda-build 24.11.2, conda-smithy 3.45.1, and conda-forge-pinning 2024.12.28.18.49.41
    
    ---------
    
    Co-authored-by: regro-cf-autotick-bot <36490558+regro-cf-autotick-bot@users.noreply.github.com>
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/build_finufft.sh
+++ recipe/build_finufft.sh
@@ -2,48 +2,11 @@
 
 set -euxo pipefail
 
-if [[ $PKG_NAME == "libfinufft" ]]; then
-
-    cmake                                     \
-        -B build-lib                          \
-        -G Ninja                              \
-        ${CMAKE_ARGS}                         \
-        -DCMAKE_BUILD_TYPE=Release            \
-        -DCMAKE_INSTALL_LIBDIR=lib            \
-        -DCMAKE_INSTALL_PREFIX=${PREFIX}      \
-        -DFINUFFT_USE_OPENMP=ON
-    cmake --build build-lib --parallel ${CPU_COUNT}
-    cmake --install build-lib
-    rm -rf ${PREFIX}/lib/libfinufft_static.a
-    rm -rf ${PREFIX}/share/finufft
-
-elif [[ $PKG_NAME == "libcufinufft" ]]; then
-
-    cmake                                     \
-        -B build-lib                          \
-        -G Ninja                              \
-        ${CMAKE_ARGS}                         \
-        -DCMAKE_BUILD_TYPE=Release            \
-        -DCMAKE_INSTALL_LIBDIR=lib            \
-        -DCMAKE_INSTALL_PREFIX=${PREFIX}      \
-        -DFINUFFT_USE_OPENMP=ON               \
-        -DFINUFFT_USE_CUDA=ON                 \
-        -DCMAKE_CUDA_ARCHITECTURES=all
-    cmake --build build-lib --parallel ${CPU_COUNT} --target cufinufft
-
-    # Install manually because CMake can't just install one target
-    cp include/cufinufft.h ${PREFIX}/include/cufinufft.h
-    cp include/cufinufft_opts.h ${PREFIX}/include/cufinufft_opts.h
-    cp include/finufft_errors.h ${PREFIX}/include/finufft_errors.h
-    cp build-lib/libcufinufft${SHLIB_EXT} ${PREFIX}/lib/libcufinufft${SHLIB_EXT}
-
-elif [[ $PKG_NAME == "finufft" ]]; then
-
-    "${PYTHON}" -m pip install --no-deps --no-build-isolation -vv ./python/finufft
-
-elif [[ $PKG_NAME == "cufinufft" ]]; then
-
-    export CUFINUFFT_DIR="${PREFIX}/lib/"
-    "${PYTHON}" -m pip install --no-deps --no-build-isolation -vv ./python/cufinufft
-
+if [[ "${target_platform}" == "osx-arm64" ]]; then
+  export ARCH_FLAGS="-mcpu=apple-m3"
+else
+  export ARCH_FLAGS="-m${ARCH}"
 fi
+
+SKBUILD_CMAKE_ARGS="-DFINUFFT_ARCH_FLAGS=${ARCH_FLAGS};-DFINUFFT_USE_OPENMP=ON" \
+  "${PYTHON}" -m pip install --no-deps --no-build-isolation -vv "./python/${PKG_NAME}"
