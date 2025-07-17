8d782943186fdd6b384808fcdc7197301394f205
Lehman Garrison <lgarrison@flatironinstitute.org>
2025-07-17 08:46:07 -0400

    cufinufft: use -arch=all-major instead of native in CI

--- recipe/build_finufft.bat
+++ recipe/build_finufft.bat
@@ -1,4 +1,4 @@
 @echo ON
 
-set "SKBUILD_CMAKE_ARGS=-DFINUFFT_USE_OPENMP=OFF;-DFINUFFT_FFTW_SUFFIX="
+set "SKBUILD_CMAKE_ARGS=-DFINUFFT_USE_OPENMP=OFF;-DFINUFFT_FFTW_SUFFIX=;-DCMAKE_CUDA_ARCHITECTURES=all-major"
 %PYTHON% -m pip install --no-deps --no-build-isolation -vv ./python/finufft
--- recipe/build_finufft.sh
+++ recipe/build_finufft.sh
@@ -8,5 +8,5 @@ else
   export ARCH_FLAGS="-m${ARCH}"
 fi
 
-SKBUILD_CMAKE_ARGS="-DFINUFFT_ARCH_FLAGS=${ARCH_FLAGS};-DFINUFFT_USE_OPENMP=ON" \
+SKBUILD_CMAKE_ARGS="-DFINUFFT_ARCH_FLAGS=${ARCH_FLAGS};-DFINUFFT_USE_OPENMP=ON;-DCMAKE_CUDA_ARCHITECTURES=all-major" \
   "${PYTHON}" -m pip install --no-deps --no-build-isolation -vv "./python/${PKG_NAME}"
